f6b35f4bf231b1d4089cda26b32e814b74e49b5d
PHMS13 <phms.13@hotmail.com>
2022-08-10 20:34:17 -0300

    quase tudo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,58 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+//interação 1//
+console.log('Enter driver name');
+
+const hacker1 = "Antonio";
+
+console.log('Enter navegator name');
+
+const hacker2 = "leando";
+
+console.log("The driver's name is",hacker1);
+
+console.log("The navegator's name is",hacker2);
+
+//interação 2//
+
+if (hacker1.length > hacker2.length) {
+
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+
+} else if (hacker1.length < hacker2.length) {
+    
+    console.log(`The navegator has the longest name, it has ${hacker2.length} characters`)
+    
+
+} else {
+
+    console.log(`Wow, you both have equally long names,${hacker2.length} characters!`)
+
+};
+
+//interação 3//
+
+const hacker3 = hacker1.toUpperCase().split("").join(" ")
+
+ console.log(hacker3);
+
+ const hacker4 = hacker1.split("").reverse().join(" ")
+
+ console.log(hacker4);
+
+ const hacker7 = [hacker1,hacker2]
 
 
-// Iteration 3: Loops
+ 
+
+ if(hacker1===0){
+
+    console.log("The driver's name goes first.");
+
+ } else if (hacker2===0){
+
+    console.log("Yo, the navigator name goes first definitely.")
+
+ } else {
+
+    console.log("What?! You both have the same name?")
+    
+ }
